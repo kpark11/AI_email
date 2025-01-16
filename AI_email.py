6942d97f9cf1215b54ef35bfa2ffaf01c21478fc
Kiman Park <kimanpark33@gmail.com>
2025-01-16 16:51:02 -0500

    modified

--- AI_email.py
+++ AI_email.py
@@ -21,7 +21,7 @@ import dash_bootstrap_components as dbc
 import plotly.graph_objs as go
 import plotly.express as px
 
-
+from bs4 import BeautifulSoup
 
 
 
@@ -78,53 +78,56 @@ def getEmail(con, start, end):
                 if msg.is_multipart():
                     # iterate over email parts
                     for part in msg.walk():
+                        
+                        # Check if there is text/html
+                        content_types = []
+                        for part2 in msg.walk():
+                            content_type = part2.get_content_type()
+                            content_types.append(content_type)
+                            
+                        
                         # extract content type of email
                         content_type = part.get_content_type()
                         content_disposition = str(part.get("Content-Disposition"))
-                        try:
-                            # get the email body
-                            body = body + part.get_payload(decode=True).decode()
-                        except:
-                            pass
-                        if content_type == "text/plain" and "attachment" not in content_disposition:
-                            # print text/plain emails and skip attachments
-                            # print("Body:", Body)
-                            '''
-                        elif "attachment" in content_disposition:
-                            # download attachment
-                            filename = part.get_filename()
-                            if filename:
-                                folder_name = clean(subject)
-                                if not os.path.isdir(folder_name):
-                                    # make a folder for this email (named after the subject)
-                                    os.mkdir(folder_name)
-                                filepath = os.path.join(folder_name, filename)
-                                # download attachment and save it
-                                open(filepath, "wb").write(part.get_payload(decode=True))
-                        '''
-                    Body.append(body)
+                        
+                        if 'text/html' in content_types:
+                            if content_type == 'text/html':
+                                try:
+                                    html = part.get_payload(decode=True).decode()
+                                except:
+                                    html = part.get_payload(decode=True).decode('unicode_escape')
+                                soup = BeautifulSoup(html, 'html.parser')
+                                body += soup.get_text()
+                                Body.append(body)
+                                break
+                            else:
+                                pass
+                        
+                        elif content_type == "text/plain":
+                            try:
+                                html = part.get_payload(decode=True).decode()
+                            except:
+                                html = part.get_payload(decode=True).decode('unicode_escape')
+                            soup = BeautifulSoup(html, 'html.parser')
+                            body += soup.get_text()
+                            Body.append(body)
+                            break
                 else:
-                    # extract content type of email
                     content_type = msg.get_content_type()
-                    # get the email body
-                    Body.append(msg.get_payload(decode=True).decode())
-                    if content_type == "text/plain":
-                        # print only text email parts
-                        # print(Body)
-                        '''
-                if content_type == "text/html":
-                    # if it's HTML, create a new HTML file and open it in browser
-                    folder_name = clean(subject)
-                    if not os.path.isdir(folder_name):
-                        # make a folder for this email (named after the subject)
-                        os.mkdir(folder_name)
-                    filename = "index.html"
-                    filepath = os.path.join(folder_name, filename)
-                    # write the file
-                    open(filepath, "w").write(body)
-                    # open in the default browser
-                    webbrowser.open(filepath)
-                '''
+                    if content_type == "text/plain" and "attachment" not in content_disposition:
+                        body += msg.get_payload(decode=True).decode()
+                        Body.append(body)
+                    elif content_type == "text/html":
+                        try:
+                            html = msg.get_payload(decode=True).decode()
+                        except:
+                            html = msg.get_payload(decode=True).decode('unicode_escape')
+                        soup = BeautifulSoup(html, 'html.parser')
+                        body += soup.get_text()
+                        Body.append(body)
+                    else:
+                        body += msg.get_payload(decode=True).decode()
+                        Body.append(body)
                 # print("="*100)
                 # print('\n\n')
     # close the connection and logout
